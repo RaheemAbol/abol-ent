ee37fcf3c9957adca5d20d9046461fa9263cf87b
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2021-11-22 12:32:19 -0500

    updated index.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "186cb5e205569a07"; }
+/******/ 		__webpack_require__.h = function() { return "96b6587436bec4f1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,20 @@
-import { Container, Box } from '@chakra-ui/react'
+import { Container, Box, Heading } from '@chakra-ui/react'
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} align="center">
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
         Hello, I&apos;m a full-stack developer based in Cincinnati!
       </Box>
 
-      <div>Hello</div>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" varient="page-title">
+            Raheem Abolfathzadeh
+          </Heading>
+          <p>Software Engineer (Designer / Designer / Artist)</p>
+        </Box>
+      </Box>
     </Container>
   )
 }
