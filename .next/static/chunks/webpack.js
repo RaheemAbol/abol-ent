74ddab2060789b5d9d37cdd96db15e74f259ac49
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2021-11-22 12:23:02 -0500

    updated box in index.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f9821d4a23492929"; }
+/******/ 		__webpack_require__.h = function() { return "186cb5e205569a07"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
-import { Container } from '@chakra-ui/react'
+import { Container, Box } from '@chakra-ui/react'
 
 const Page = () => {
-  return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} align="center">
+        Hello, I&apos;m a full-stack developer based in Cincinnati!
+      </Box>
+
+      <div>Hello</div>
+    </Container>
+  )
 }
 export default Page
