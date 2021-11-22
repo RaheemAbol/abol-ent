a561b32f2c8824e882b85cac70c51bc8daa378e4
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2021-11-22 17:36:44 -0500

    completed imports for navbar

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "96b6587436bec4f1"; }
+/******/ 		__webpack_require__.h = function() { return "c525172be30ce035"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
