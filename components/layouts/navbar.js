266bdec09cda6f5f13bd50f0059b18ef93e7e8a4
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2021-11-23 15:50:31 -0500

    added nav background

--- components/layouts/navbar.js
+++ components/layouts/navbar.js
@@ -20,4 +20,16 @@ import { HamburgerIcon } from '@chakra-ui/icons'
 const LinkItem = ({ href, path, children }) => {
   const active = path === href
   const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
+
+  return (
+    <NextLink href={href}>
+      <Link
+        p={2}
+        bg={active ? 'glassTeal' : undefined}
+        color={active ? '#202023' : inactiveColor}
+      >
+        {children}
+      </Link>
+    </NextLink>
+  )
 }
