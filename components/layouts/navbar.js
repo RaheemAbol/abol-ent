f3cc1eb561a2877c0bd2e975eaeed6ed8ddc10b0
Raheem Abolfathzadeh <raheemabol91@gmail.com>
2021-11-30 10:56:01 -0500

    updated navbar with NextLink to View source

--- components/layouts/navbar.js
+++ components/layouts/navbar.js
@@ -94,6 +94,9 @@ const Navbar = props => {
                 <NextLink href="/posts" passHref>
                   <MenuItem as={Link}>Posts</MenuItem>
                 </NextLink>
+                <MenuItem as={Link} href="https://github.com/RaheemAbol">
+                  View Source
+                </MenuItem>
               </MenuList>
             </Menu>
           </Box>
